fffe3ba8d16e6ac1aabe9a5a4e591bd08a9e2c55
Jose-Lema <jose.lema.2310@gmail.com>
2024-06-16 21:31:49 -0700

    Added env file and API Key

--- flixx-app-theme/js/script.js
+++ flixx-app-theme/js/script.js
@@ -1,43 +1,49 @@
-const global = {
-    currentPage: window.location.pathname,
-};
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
+const api_key = process.env.API_KEY;
 
-function highlightActiveLink(){
-    const links = document.querySelectorAll('.nav-link');
-    links.forEach((link) => {
-        if(link.getAttribute('href') === global.currentPage){
-            link.classList.add('active');
-        }
-    });
-}
+console.log(api_key);
+
+// const global = {
+//     currentPage: window.location.pathname,
+// };
+
+// function highlightActiveLink(){
+//     const links = document.querySelectorAll('.nav-link');
+//     links.forEach((link) => {
+//         if(link.getAttribute('href') === global.currentPage){
+//             link.classList.add('active');
+//         }
+//     });
+// }
 
 
-//Make sure your VS Code is opened to the innermost folder with everything in it
-function init() {
-    switch(global.currentPage) {
-        case '/':
-        case '/index.html':
-            console.log('Home');
-            break;
+// //Make sure your VS Code is opened to the innermost folder with everything in it
+// function init() {
+//     switch(global.currentPage) {
+//         case '/':
+//         case '/index.html':
+//             console.log('Home');
+//             break;
         
-        case '/shows.html':
-            console.log('TV Shows');
-            break;
+//         case '/shows.html':
+//             console.log('TV Shows');
+//             break;
         
-        case '/movie-details.html':
-            console.log('Movie Details');
-            break;
+//         case '/movie-details.html':
+//             console.log('Movie Details');
+//             break;
         
-        case '/tv-details.html':
-            console.log('Show Details');
-            break;
+//         case '/tv-details.html':
+//             console.log('Show Details');
+//             break;
         
-        case '/search.html':
-            console.log('Search');
-            break;        
-    }
+//         case '/search.html':
+//             console.log('Search');
+//             break;        
+//     }
 
-    highlightActiveLink();
-}
+//     highlightActiveLink();
+// }
 
-document.addEventListener('DOMContentLoaded', init);
+// document.addEventListener('DOMContentLoaded', init);
